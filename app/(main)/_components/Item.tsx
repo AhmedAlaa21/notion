3bfdcbde801bfb4e0eaf0d8f5130639201bc8a7e
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-10-21 07:26:38 +0300

    update search command

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -93,6 +93,15 @@ export const Item = ({
     });
   };
 
+  const userAgent = window.navigator.userAgent;
+
+  const getOS = () => {
+    if (userAgent.includes("Mac")) return "⌘";
+    else {
+      return "ctrl";
+    }
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
@@ -124,7 +133,7 @@ export const Item = ({
       <span className="truncate">{label}</span>
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>K
+          <span className="text-xs">{getOS()}</span>K
         </kbd>
       )}
       {!!id && (
